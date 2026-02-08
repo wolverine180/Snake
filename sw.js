c12e5bda017417d687fa32e42cf088f471841d24
wolverine180 <98281898+wolverine180@users.noreply.github.com>
2026-02-08 22:22:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "snake-pwa-v1";
+const CACHE_NAME = "snake-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -28,3 +28,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
+
